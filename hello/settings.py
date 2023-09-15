9fbb24811bfaa99174ca7c485ee82fdadc4a93f9
kartavya512 <102538285+kartavya512@users.noreply.github.com>
2023-09-15 15:10:46 +0530

    Update settings.py

--- hello/settings.py
+++ hello/settings.py
@@ -136,7 +136,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'docnow55@gmail.com'
-EMAIL_HOST_PASSWORD = 'owxtwogrzhuomjhf'
+EMAIL_HOST_PASSWORD = ''
 
 
 
